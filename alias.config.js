62feed0b4cd5bcb29fa9d1add83a09931a41c685
yujinpan <1192878390@qq.com>
2020-09-08 20:14:08 +0800

    feat: add declaration build from Gulp(support .vue file)

--- alias.config.js
+++ alias.config.js
@@ -1,5 +1,24 @@
 const path = require('path');
+const tsPaths = require('./tsconfig.json').compilerOptions.paths;
 
-module.exports = {
-  '@': path.resolve(__dirname, 'src/')
-};
+// read tsconfig.json
+const aliasConfig = {};
+for (let key in tsPaths) {
+  if (tsPaths[key].length > 1) {
+    console.warn(
+      'alias.config.js warn:',
+      'tsconfig.json paths value must be only one.\n'
+    );
+  }
+  // must use root path
+  aliasConfig[transformPath(key)] =
+    path.resolve('./') + '/' + transformPath(tsPaths[key][0]);
+}
+
+console.log('alias.config.js result：', aliasConfig, '\n');
+
+module.exports = aliasConfig;
+
+function transformPath(path) {
+  return path.replace('/*', '');
+}
---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const through = require('through2');
+const fs = require('fs');
+const path = require('path');
+const aliasConfig = require('./alias.config');
+const { parseComponent } = require('vue-template-compiler');
+
+function build() {
+  // clear types folder first
+  fs.rmdirSync('types', { recursive: true });
+
+  return gulp
+    .src(['./src/**/*.ts', './src/**/*.vue'])
+    .pipe(
+      through.obj(function(file, _, cb) {
+        // get scripts
+        if (file.extname === '.vue') {
+          const code = file.contents.toString();
+          const scripts = parseComponent(code);
+          file.contents = Buffer.from(scripts.script.content.trim());
+          file.extname = '.vue.ts';
+        }
+        file.contents = Buffer.from(
+          transformToRelativePath(
+            file.contents.toString(),
+            path.resolve(file.path, '../')
+          )
+        );
+        cb(null, file);
+      })
+    )
+    .pipe(ts.createProject('./tsconfig.json')())
+    .dts.pipe(gulp.dest('types'));
+}
+
+// transform absolute path to relative path
+function transformToRelativePath(codes, filepath) {
+  const imports = codes.match(/(from\s|require\(|import\()'@\/[^']*'\)?/g);
+  if (imports) {
+    // get source path
+    const paths = imports.map((item) =>
+      item.replace(/^(from\s|require\(|import\()'/, '').replace(/'\)?/, '')
+    );
+
+    // get relative path
+    const relativePaths = paths
+      .map((item) => {
+        // read alias path config
+        for (let key in aliasConfig) {
+          if (item.startsWith(key)) {
+            return path.relative(
+              filepath,
+              item.replace(new RegExp('^' + key), aliasConfig[key])
+            );
+          }
+        }
+        console.warn(
+          'gulpfile.js warn:',
+          'can not find the path config:' + item
+        );
+        return item;
+      })
+      // add ./
+      .map((item) => (item.startsWith('.') ? item : './' + item));
+
+    // replace source code
+    paths.forEach((item, index) => {
+      codes = codes.replace(item, relativePaths[index]);
+    });
+  }
+  return codes;
+}
+
+module.exports.default = build;
