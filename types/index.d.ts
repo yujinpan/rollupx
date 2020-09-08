e98bd9c22d39f1e65608ba6f0679cc083ce8fa8f
yujinpan <1192878390@qq.com>
2020-09-08 20:14:23 +0800

    build & build types

---
+++ types/index.d.ts
@@ -0,0 +1,3 @@
+import HelloWorld from './components/HelloWorld.vue';
+import './styles/index.scss';
+export default HelloWorld;
---
+++ types/utils/check-value.d.ts
@@ -0,0 +1,12 @@
+/**
+ * 校验数组是否有效
+ * @param {*} data
+ * @return {boolean}
+ */
+export declare function isValidArray(data: any): number;
+/**
+ * 校验一个简单值
+ * @param {*} value
+ * @return {boolean}
+ */
+export declare function isValidValue(value: any): boolean;
