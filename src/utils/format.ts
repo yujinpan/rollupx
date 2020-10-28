02c20571a448ae29afeff0f938cd5b5ce6147aa6
yujinpan <1192878390@qq.com>
2020-10-28 13:54:55 +0800

    feat: add types.ts and `getValueByKeyPath` method

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,4 +1,5 @@
 import { isValidValue } from '@/utils/validate';
+import { Obj } from '@/types';
 
 /**
  * 合并有效的对象
@@ -44,11 +45,26 @@ export function getValidObj<T extends Object>(obj: T): Partial<T> {
  * @param {Function} cb example: (item) => ({ [item.id]: item.value })
  * @returns Object
  */
-export function arrayReduce<T>(
-  arr: T[],
-  cb: (p: T) => Object
-): { [p: string]: any } {
+export function arrayReduce<T>(arr: T[], cb: (p: T) => Object): Obj {
   return arr.reduce((prev, next) => {
     return { ...prev, ...cb(next) };
   }, {});
+}
+
+/**
+ * 获取字符串的键值 obj['a.b.c'] = obj.a.b.c
+ * @param {object} obj
+ * @param {string} strKeys
+ */
+export function getValueByKeyPath(obj: Obj, strKeys: string) {
+  if (isValidValue(obj[strKeys])) {
+    return obj[strKeys];
+  }
+  const keys = strKeys.split('.');
+  let value = obj;
+  for (let i = 0; i < keys.length; i++) {
+    if (!isValidValue(value)) break;
+    value = value[keys[i]];
+  }
+  return value;
 }
