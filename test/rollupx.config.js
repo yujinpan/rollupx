2ce9b7db2b1c493b380f3c058abe39bc83d1d024
yujinpan <1192878390@qq.com>
2023-03-17 11:43:43 +0800

    test: update test scripts and config

--- test/rollupx.config.js
+++ test/rollupx.config.js
@@ -5,6 +5,15 @@ module.exports = {
     test: './',
   },
 
+  formats: [
+    { format: 'es' },
+    { format: 'esm', inputFiles: ['index.ts'], outputFile: '/esm/[name][ext]' },
+    { format: 'esm', inputFiles: ['index.ts'], outputFile: '[name].esm.js' },
+    { format: 'cjs', inputFiles: ['index.ts'], outputFile: '/cjs/[name][ext]' },
+    { format: 'cjs', inputFiles: ['index.ts'], outputFile: '[name].cjs.js' },
+    { format: 'umd', inputFiles: ['index.ts'], outputFile: '[name].umd[ext]' },
+  ],
+
   // format: 'umd',
   // external: ['vue'],
   // outputName: 'MyBundle',
