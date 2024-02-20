e6e83373148a6742f2f3138d1c824a083ceb7d26
yujinpan <1192878390@qq.com>
2024-02-20 10:17:42 +0800

    feat: support ESM module config file

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,36 @@
 import fs from 'fs';
+import path from 'path';
 
 import { build, watch } from './build';
+import { Options } from './config';
+import { printErr } from './utils';
 
-const configPath = process.cwd() + '/rollupx.config.js';
-if (fs.existsSync(configPath)) {
-  import(configPath).then((res) => {
-    res = { ...res };
-    res.watch || process.argv.includes('--watch') ? watch(res) : build(res);
-  });
-} else {
-  build();
+readConfig().then((config) => {
+  if (config?.watch || process.argv.includes('--watch')) {
+    watch(config);
+  } else {
+    build(config);
+  }
+});
+
+async function readConfig(): Promise<Options> {
+  let configPath = path.resolve(process.cwd(), 'rollupx.config.js');
+  if (isESM()) {
+    configPath = path.resolve(process.cwd(), 'rollupx.config.cjs');
+    if (!fs.existsSync(configPath)) {
+      printErr('ESM module must be use rollupx.config.cjs');
+      return Promise.reject();
+    }
+  }
+  if (fs.existsSync(configPath)) {
+    return import(configPath).then((res) => ({ ...res }));
+  }
+}
+
+function isESM() {
+  const pkPath = path.resolve(process.cwd(), 'package.json');
+
+  const pk = JSON.parse(fs.readFileSync(pkPath).toString());
+
+  return pk.type === 'module';
 }
